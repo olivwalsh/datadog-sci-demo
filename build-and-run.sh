a03f64a1e6660feb14ab76d49269b88b2d1cae4b
Olivia Walsh <olivia.walsh@datadoghq.com>
2025-06-19 19:42:58 +0200

    test building app as per doc

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # APM Telemetry Tagging - Set Git metadata for container builds
 export DD_GIT_COMMIT_SHA=$(git rev-parse HEAD)
-docker compose up
+echo $DD_GIT_COMMIT_SHA
+docker compose up --build
--- node-server/server.ts
+++ node-server/server.ts
@@ -4,20 +4,24 @@ import express from 'express';
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log('helloooooooooo');
   res.send("Hello Express app!");
 });
 
 app.get("/trigger-error", (req, res) => {
+  console.log('helloooooooooo');
+  console.log('DD_GIT_COMMIT_SHA:', process.env.DD_GIT_COMMIT_SHA);
+  console.log('DD_GIT_REPOSITORY_URL:', process.env.DD_GIT_REPOSITORY_URL);
   function throwError() {
     actuallyThrowError()
   }
   function actuallyThrowError() {
-    throw Error("error thrown");
+    throw Error("hello error thrown");
   }
 
-  res.send(`${throwError()}`);
+  res.send(`hello ${throwError()}`);
 });
 
 app.listen(8080, () => {
-  console.log("server started");
+  console.log("server started aaaaaaaa");
 });
